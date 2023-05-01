6071fa686a959acdf223be12965f4cb85fc73411
Arnold Palmer <rubenruiz@Rubens-MBP.lan>
2023-05-01 06:58:30 -0700

    adding changes to script (still not working)

--- script.js
+++ script.js
@@ -8,29 +8,12 @@ var lowercaseLetters = ["a","b","c","e","f","g","h","i","j","k","l","m","n","o",
 var numbers = ["0","1","2","3","4","5","6","7","8","9"] // numbers
 var specialCharacters = ["!","@","#","$","%","^","&","*","(",")","_","+","~","`","|","}","{","[","]",":",";","?",">","<","/","-","=",";"] // special characters
 
-var chosenCharacters = "";
-
-
-if (userChoiceSpecialCharacters=true) {
-chosenCharacters += specialCharacters
-}
-
-if (userChoiceNumbers=true) {
-chosenCharacters += numbers
-}
-if (userChoiceLowerCase=true) {
-  chosenCharacters += lowercaseLetters
-  }
-   if (userChoiceUpperCase=true) {
-    chosenCharacters += capitalLetters
-    }
 
 
 //make 3 prompts
 
-randomLetter = chosenCharacters[Math.floor(Math.random() * chosenCharacters.length)];
 
-console.log(randomLetter + " is the random letter.")
+
 
 var userChoiceHowMany = window.prompt("How many characters? 8-28");
 var userChoiceNumbers = window.confirm("Numbers?");
@@ -38,19 +21,38 @@ var userChoiceSpecialCharacters = window.confirm("Special Characters?");
 var userChoiceLowerCase = window.confirm("Lowercase?");
 var userChoiceUpperCase = window.confirm("Uppercase?");
 
+var chosenCharacters = "";
+
+if ((userChoiceSpecialCharacters==true)) {
+  chosenCharacters += specialCharacters;
+} //if user chooses special characters, adds special characters to var chosenCharacters 
+else if ((userChoiceNumbers==true)) {
+  chosenCharacters += numbers;
+} //if user chooses numbers, adds numbers s to var chosenCharacters 
+else if ((userChoiceLowerCase==true)) {
+  chosenCharacters += lowercaseLetters;
+} //if user chooses lower case letters, adds lower case letters to var chosenCharacters 
+else if ((userChoiceUpperCase==true)) {
+  chosenCharacters += capitalLetters;
+} //if user chooses upper case letters, adds upper case letters to var chosenCharacters 
+
+randomLetter=chosenCharacters[Math.floor(Math.random() * chosenCharacters.length)]; // should choose a random character from the chosen characters (prompts) 
+
+console.log(randomLetter + " is the random letter.") // console logs a random character from the chosencharacters
+
+
+
+
 function generatePassword() {
   
 
   
 
+  console.log(capitalLetters[0]) // button clicked
 
 
   }
-  // if userChoiceNumbers = confirmed then generate a password the specified length and a number
-  // if userChoiceSpecialCharacters = confirmed then generate a passdword with special characters
-  // if userChoiceLowerCase = confirmed then generate a passdword with Lower Case letters
-  // if userChoiceUpperCase = confirmed then generate a passdword with Upper Case letters 
- // if userChoiceHowMany = a number then generate a password the length of choice
+
 
 
 
@@ -58,12 +60,7 @@ function generatePassword() {
 
 
 
-  console.log(capitalLetters[0]) // button clicked
-
-  //prompt for how many letters
-  //prompt if i need upper case letters
-  //prompt if i need lower case letters
-  //function to pull random letters from the var's to meet the users the specific needs from prompts
+  //function to pull random letters from the chosenCharacters's to meet the users the specific needs from prompts
 
 
 // Get references to the #generate element
@@ -89,6 +86,3 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-
-for (let i=0; i < userChoiceHowMany; i++)
-  console.log (randomLetter[2])
