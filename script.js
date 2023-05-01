0cb65c900c9682e4135b635dd5fd15a0297dc697
Arnold Palmer <rubenruiz@Rubens-MBP.lan>
2023-05-01 11:49:07 -0700

    adding changes recommended from tutor

--- script.js
+++ script.js
@@ -3,17 +3,17 @@
 
 
 
-var capitalLetters = ["A","B","C","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]; // uppercase letters
-var lowercaseLetters = ["a","b","c","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];  // lowercase letters
-var numbers = ["0","1","2","3","4","5","6","7","8","9"] // numbers
-var specialCharacters = ["!","@","#","$","%","^","&","*","(",")","_","+","~","`","|","}","{","[","]",":",";","?",">","<","/","-","=",";"] // special characters
+var capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // uppercase letters
+var lowercaseLetters = "abcefghijklmnopqrstuvwxyz";  // lowercase letter
+var numbers = "0123456789" // numbers
+var specialCharacters = "!@#$%^&*()_+~`|}{[]:;?></-=;" // special characters
 
 
 
 //make 3 prompts
 
 
-
+function generatePassword() {
 
 var userChoiceHowMany = window.prompt("How many characters? 8-28");
 var userChoiceNumbers = window.confirm("Numbers?");
@@ -26,13 +26,13 @@ var chosenCharacters = "";
 if ((userChoiceSpecialCharacters==true)) {
   chosenCharacters += specialCharacters;
 } //if user chooses special characters, adds special characters to var chosenCharacters 
-else if ((userChoiceNumbers==true)) {
+ if ((userChoiceNumbers==true)) {
   chosenCharacters += numbers;
 } //if user chooses numbers, adds numbers s to var chosenCharacters 
-else if ((userChoiceLowerCase==true)) {
+ if ((userChoiceLowerCase==true)) {
   chosenCharacters += lowercaseLetters;
 } //if user chooses lower case letters, adds lower case letters to var chosenCharacters 
-else if ((userChoiceUpperCase==true)) {
+ if ((userChoiceUpperCase==true)) {
   chosenCharacters += capitalLetters;
 } //if user chooses upper case letters, adds upper case letters to var chosenCharacters 
 
@@ -40,18 +40,18 @@ randomLetter=chosenCharacters[Math.floor(Math.random() * chosenCharacters.length
 
 console.log(randomLetter + " is the random letter.") // console logs a random character from the chosencharacters
 
+console.log(capitalLetters[0]) // button clicked
+
+}
 
 
 
-function generatePassword() {
+
   
 
   
 
-  console.log(capitalLetters[0]) // button clicked
 
-
-  }
 
 
 
