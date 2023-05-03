559ab11df6554ddb37af9f8793d7f347a3f2a46d
Arnold Palmer <rubenruiz@Rubens-MBP.lan>
2023-05-03 07:10:23 -0700

    adding near complete generator with broken while loop

--- script.js
+++ script.js
@@ -1,66 +1,52 @@
 // Assignment code here
 
-
-
-
-var capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // uppercase letters
-var lowercaseLetters = "abcefghijklmnopqrstuvwxyz";  // lowercase letter
-var numbers = "0123456789" // numbers
-var specialCharacters = "!@#$%^&*()_+~`|}{[]:;?></-=;" // special characters
-
-
-
-//make 3 prompts
-
+var capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Uppercase Letters
+var lowercaseLetters = "abcefghijklmnopqrstuvwxyz";  // Lowercase Letter
+var numbers = "0123456789" // Numbers
+var specialCharacters = "!@#$%^&*()_+~`|}{[]:;?></-=;" // Special Characters
+var finalpassword = [""] // placeholder var the end result Password user will see
 
 function generatePassword() {
+// prompts password criteria loop
 
-var userChoiceHowMany = window.prompt("How many characters? 8-28");
-var userChoiceNumbers = window.confirm("Numbers?");
-var userChoiceSpecialCharacters = window.confirm("Special Characters?");
-var userChoiceLowerCase = window.confirm("Lowercase?");
-var userChoiceUpperCase = window.confirm("Uppercase?");
+var userChoiceHowMany = window.prompt("How many character? minimum = 8, maximum = 128"); // User confirms how many characters for password 
 
-var chosenCharacters = "";
+while (userChoiceHowMany < 8,userChoiceHowMany > 128){
+    userChoiceHowMany++; 
+    window.prompt("How many character? minimum = 8, maximum = 128");
+}
+
+
+var userChoiceNumbers = window.confirm("Would you like Numbers?"); // User confirms if they want Numbers in password 
+var userChoiceSpecialCharacters = window.confirm("Would you like Special Characters?"); // User confirms if they want Special Characters in password 
+var userChoiceLowerCase = window.confirm("Would you like Lowercase letters?"); // User confirms if they want Lower Case letters in password
+var userChoiceUpperCase = window.confirm("Would you like Uppercase letters?"); // User confirms if they want Upper Case Letters in password
+
+var chosenCharacters = ""; // Place hodler var for the confirmed characters for password
+var actualpassword = []; // Place holder var for a random array of characters by created by "for loop" below
 
 if ((userChoiceSpecialCharacters==true)) {
-  chosenCharacters += specialCharacters;
-} //if user chooses special characters, adds special characters to var chosenCharacters 
- if ((userChoiceNumbers==true)) {
-  chosenCharacters += numbers;
-} //if user chooses numbers, adds numbers s to var chosenCharacters 
- if ((userChoiceLowerCase==true)) {
-  chosenCharacters += lowercaseLetters;
-} //if user chooses lower case letters, adds lower case letters to var chosenCharacters 
- if ((userChoiceUpperCase==true)) {
-  chosenCharacters += capitalLetters;
-} //if user chooses upper case letters, adds upper case letters to var chosenCharacters 
+ chosenCharacters += specialCharacters;} // if User confirms they want Special Characters, this adds Special Characters to var place holder var "chosenCharacters"
+if ((userChoiceNumbers==true)) {
+ chosenCharacters += numbers;} // if User confirms they want Numbers, this adds Numbers to var place holder var "chosenCharacters"
+if ((userChoiceLowerCase==true)) {
+ chosenCharacters += lowercaseLetters;} // if User confirms they want Lower Case Letters, this adds Lower Case Letter to var "chosenCharacters"
+if ((userChoiceUpperCase==true)) {
+ chosenCharacters += capitalLetters;} // if User confirms they want Upper Case Letters, this adds Upper Case Letters to var "chosenCharacters"
 
-randomLetter=chosenCharacters[Math.floor(Math.random() * chosenCharacters.length)]; // should choose a random character from the chosen characters (prompts) 
-var i = ""
+// var "randomLetter" is a random character from the "chosenCharacters" placeholder var created from math random method
 for (let i = 0; i < userChoiceHowMany; i++) {
+  var randomLetter =
+    chosenCharacters[Math.floor(Math.random() * chosenCharacters.length)]; // for loop repeats the random math method X amount of times (how many desired characters for password confirmed by user) and stores them into the "actuapassword" var as an array
 
-  console.log(randomLetter = " is the password");
+  actualpassword.push(randomLetter);
+  }
+finalpassword = actualpassword.join(""); // adds the resulted "array" of "actualpassword" to globally scoped var "finalpassword" as a string to eventually be shown on the page
+return finalpassword;
 }
-
-
-}
-
-
-
-
-  
-
-
-
-  //function to pull random letters from the chosenCharacters's to meet the users the specific needs from prompts
-
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-
-
+var generateBtn = document.querySelector("#generate"); 
 
 // Write password to the #password input
 function writePassword() {
@@ -71,11 +57,5 @@ function writePassword() {
 
 }
 
-
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
